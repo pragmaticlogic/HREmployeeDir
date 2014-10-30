100cb77d07ef7ff8c731656072fd8828957bafc4
Kevin Le <pragmaticlogic@gmail.com>
2014-10-30 10:20:14 -0500

    Add employee controller test

---
+++ SimpleHR.Tests/Controllers/EmployeesControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleHR;
+using SimpleHR.Controllers;
+using PagedList;
+
+namespace SimpleHR.Tests.Controllers
+{
+    [TestClass]
+    public class EmployeesControllerTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            // Arrange
+            EmployeesController controller = new EmployeesController();
+
+            // Act
+            ViewResult result = controller.Index("", "", "", null) as ViewResult;
+            PagedList.IPagedList<SimpleHR.Models.Employee> employees = result.Model as PagedList.IPagedList<SimpleHR.Models.Employee>;
+
+            // Assert
+            Assert.IsNotNull(employees.Count > 0);
+        }
+    }
+}
